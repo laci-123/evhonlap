62cdf5cecdee7938e07070000411fb135916b5ae
Gohér László <goher.laci@gmail.com>
2021-01-25 17:06:14 +0100

    Removed all unecessary constants from 'gondolatok.php' (and fixed indentation)

--- script/gondolatok.php
+++ script/gondolatok.php
@@ -1,64 +1,64 @@
 <?php
 function gondolatok()
 {	
-	$titles = array();
-	$content = "";
-	$entry = 0;
-	$actual_content = "";
-	$output = "";
-	
-	$output .= SEGMENT_CONTENTHEADER;
-	$output .= SEGMENT_THOUGHTS_TITLE;
-	
-	try{
-		$content = file_get_contents_safe(FILE_THOUGHTS_CONTENT);
-	}
-	catch(Exception $ex){
-		$output .= ERROR_NOT_ACCESSIBLE;
-		$output .= SEGMENT_CONTENTFOOTER;
-		return $output;
-	}
-	
-	preg_match_all(REGEX_ENTRY, $content, $titles);
-	
-	if(count($titles[1]) == 0)
-	{
-		$output .= MESSAGE_EMPTY_SECTION;
-		$output .= SEGMENT_CONTENTFOOTER;
-		return $output;
-	}
-	
-	$output .= "<p>";
-	for($i = count($titles[1]); $i > 0; $i--)
-	{
-		$output .= "<a class='belso_link' href='?hely=gondolatok&cim=".$i."'>".$titles[1][count($titles[1])-$i]."</a><br>\n";
-	}
-	$output .= "</p>";
-	$output .= SEGMENT_SEPARATOR;
-	
-	try{
-		$entry = count($titles[1]) - GETparameters::get_int(GET_VALUE_THOUGHTS_TITLE) + 1;
-	}
-	catch(OutOfBoundsException $ex){
-		$entry = 1;
-	}
-	
-	
-	if($entry != count($titles[1]))
-	{
-		$actual_content = get_string_between($content, $titles[1][$entry-1], $titles[1][$entry]);
-	}
-	else
-	{
-		$actual_content = get_string_between($content, $titles[1][$entry-1], "</article>");
-	}
-	$actual_content = str_replace("<!--§", "", $actual_content);
-	$actual_content = str_replace("§-->", "", $actual_content);
-	$actual_content = preg_replace("/<.r>/", "", $actual_content);
-	$output .= $actual_content;
-	
-	$output .= SEGMENT_CONTENTFOOTER;
-	
+    $titles = array();
+    $content = "";
+    $entry = 0;
+    $actual_content = "";
+    $output = "";
+    
+    $output .= "<article class='content'>\n";
+    $output .= "<h2>Gondolatok</h2>";
+    
+    try{
+	$content = file_get_contents_safe("content/gondolatok.html");
+    }
+    catch(Exception $ex){
+	$output .= "<p>Ez a tartalom egy váratlan hiba miatt jelenleg nem érhető el. </p>\n";
+	$output .= "</article>\n";
 	return $output;
+    }
+    
+    preg_match_all("/<!\-\-§(.*?)§\-\->/s", $content, $titles);
+    
+    if(count($titles[1]) == 0)
+    {
+	$output .= "<p>Ez a szakasz jelenleg üres. </p>";
+	$output .= "</article>\n";
+	return $output;
+    }
+    
+    $output .= "<p>";
+    for($i = count($titles[1]); $i > 0; $i--)
+    {
+	$output .= "<a class='belso_link' href='?hely=gondolatok&cim=".$i."'>".$titles[1][count($titles[1])-$i]."</a><br>\n";
+    }
+    $output .= "</p>";
+    $output .= "<hr id='elvalaszto'>";
+    
+    try{
+	$entry = count($titles[1]) - GETparameters::get_int("cim") + 1;
+    }
+    catch(OutOfBoundsException $ex){
+	$entry = 1;
+    }
+    
+    
+    if($entry != count($titles[1]))
+    {
+	$actual_content = get_string_between($content, $titles[1][$entry-1], $titles[1][$entry]);
+    }
+    else
+    {
+	$actual_content = get_string_between($content, $titles[1][$entry-1], "</article>");
+    }
+    $actual_content = str_replace("<!--§", "", $actual_content);
+    $actual_content = str_replace("§-->", "", $actual_content);
+    $actual_content = preg_replace("/<.r>/", "", $actual_content);
+    $output .= $actual_content;
+    
+    $output .= "</article>\n";
+    
+    return $output;
 }
 ?>
