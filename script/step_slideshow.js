e3fe3a26a8c8f8b01a1c2d7ae51fcde366257315
Gohér László <goher.laci@gmail.com>
2022-01-26 17:39:52 +0100

    Fixed slideshow autoplay and manual stepping interfering with each other

--- script/step_slideshow.js
+++ script/step_slideshow.js
@@ -1,18 +1,58 @@
 let next = document.getElementById("next_slide");
+let prev = document.getElementById("prev_slide");
 let container = document.getElementById("slideshow_container");
+let slide = document.getElementById("the_slide");
+
+let timeout_set = false;
+let click_by_user = true;
+let stopped = false;
 
 function auto_next_slide(){
+    timeout_set = false;
+    
+    if(stopped){
+	return;
+    }
+    
     container.classList.add("slideshow_transition");
     setTimeout(function(){
+	click_by_user = false
 	next.click();
+	click_by_user = true;
     }, 250);
     setTimeout(function(){
 	container.classList.remove("slideshow_transition");
     }, 500);
 }
 
-document.getElementById("the_slide").addEventListener("load", function(){
+function user_click(){
+    if(click_by_user){
+	if(stopped){
+	    return;
+	}
+	
+	stopped = true;
+	setTimeout(function(){
+	    stopped = false;
+	    auto_next_slide();
+	}, 20000);
+    }
+}
+
+slide.addEventListener("load", function(){
+    if(!timeout_set){
+	timeout_set = true;
+	setTimeout(function(){
+	    auto_next_slide();
+	}, 5000);
+    }
+});
+
+next.addEventListener("click", user_click);
+prev.addEventListener("click", user_click);
+
+if(!timeout_set){
     setTimeout(function(){
 	auto_next_slide();
     }, 5000);
-});
+}
