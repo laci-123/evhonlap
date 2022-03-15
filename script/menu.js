9db79f9692c5fc43797bc54fea635febae990e4f
Gohér László <goher.laci@gmail.com>
2022-03-15 14:38:53 +0100

    Auto next slide does not interfer with menu closing script

--- script/menu.js
+++ script/menu.js
@@ -2,6 +2,8 @@ let trigger = document.getElementById("trigger");
 document.addEventListener("click", function (event) {
     if (event.target.closest("#menu_lista") ||
         event.target.closest("#trigger") ||
+        event.target.closest("#next_slide") ||
+        event.target.closest("#prev_slide") ||
         event.target.tagName == "LABEL" ||
         event.target.tagName == "SPAN") 
     {
