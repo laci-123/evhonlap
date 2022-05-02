dac7e40f39591ac1620d8e920bffb17381a4f5a4
Gohér László <goher.laci@gmail.com>
2022-05-02 22:36:33 +0200

    Removed local scripts, do not need them since started using IDEs

--- script_local/DatumFeltolto.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-FILE='/home/laci/Documents/evhonlap/index.php'
-
-now="$(date +'%Y.%m.%d')"
-sed  -i "s/.*Utoljára frissítve.*/Utoljára frissítve: $now/" "$FILE"
-./FileFeltolto.sh index.php
