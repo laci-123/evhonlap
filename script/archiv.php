4dc74331099da6f2ba8633931ebf3c1e52746440
Gohér László <goher.laci@gmail.com>
2022-07-23 12:54:39 +0200

    started rewriting archiv.php

--- script/archiv.php
+++ script/archiv.php
@@ -1,5 +1,18 @@
 <?php
 $get_content = function(){
-    echo file_get_contents_safe("content/archiv.html");
+    $output = "<h2>Régebbi események</h2>\n";
+    $files = scandir_safe_compact("content/archiv/");
+    for($i = count($files) - 1; $i >= 0; --$i){
+        $file = $files[$i];
+        $name = substr($file, 0, strpos($file, "."));
+        $output .= "<a href='?hely=hir&cim=$name' class='link_box'>\n";
+        $output .= "<img src='img/cikk/$name.png' alt=''>\n";
+        $file_content = file_get_contents_safe("content/archiv/$file");
+        $title = get_string_between($file_content, "<h3>", "</h3>");
+        $output .= "<span>$title</span>\n";
+        $output .= "</a>";
+    }
+
+    return $output;
 };
 ?>
