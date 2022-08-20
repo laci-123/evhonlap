5e102af2c1eee32445843ae5b96730bf6c3b2474
Gohér László <goher.laci@gmail.com>
2022-08-20 09:18:17 +0200

    content in 'content/aktualis' can be ordered

--- script/aktualis.php
+++ script/aktualis.php
@@ -5,7 +5,8 @@ $get_content = function(){
     foreach($files as $file){
         $name = substr($file, 0, strpos($file, "."));
         $output .= "<a href='?hely=esemeny&cim=$name' class='link_box aktualis_box'>\n";
-        $output .= "<img src='img/cikk/$name.png' alt=''>\n";
+        $img_name = preg_replace("/^[0-9]*_/", "", $name);
+        $output .= "<img src='img/cikk/$img_name.png' alt=''>\n";
         $file_content = file_get_contents_safe("content/aktualis/$file");
         $title = get_string_between($file_content, "<h2>", "</h2>");
         $output .= "<span>$title</span>\n";
