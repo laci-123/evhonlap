ce6bfd074f98422bfc7f69cb47adf447b97e745e
Gohér László <goher.laci@gmail.com>
2022-09-04 12:10:49 +0200

    reordered contnet on main page

--- script/fooldal.php
+++ script/fooldal.php
@@ -1,8 +1,10 @@
 <?php 
 $get_content = function(){
+    // ======= Üdvözlés =========
     $output =  file_get_contents_safe("content/fooldal.html");
     $output .= "<hr>\n";
 
+    // ======= Aktuális alkalmak =========
     $files = scandir_safe_compact("content/aktualis/");
     foreach($files as $file){
         $name = substr($file, 0, strpos($file, "."));
@@ -16,28 +18,11 @@ $get_content = function(){
     }
     $output .= "<hr>\n";
 
+    // ======= Ukrajna ========
     $output .=  file_get_contents_safe("content/ukrajna_fooldal.html");
     $output .= "<hr>\n";
 
-    $output .= "<div id='fooldal_kepek'>\n";
-    if(include "slideshow.php"){
-	$galery = "img/galeria/";
-	//TODO: automate
-	$folder = "keszohidegkut_2022/";
-	$album = 41;
-	$title = "Keszőhidegkúti tábor 2022";
-	try{
-	    $files = scandir_safe_compact($galery.$folder);
-        $output .= "<h3>$title</h3>";
-        $output .= slideshow($galery, $folder, $files, "", 0);
-	    $output .= "<a href='?hely=galeria&album=$album' id='fooldal_kepek_link' title='$title'></a>\n";
-	}
-	catch(Exception $ex){
-            $output .= "<b>Error: </b>".$ex->getMessage();
-	}
-    }
-    $output .= "</div>\n<hr>\n";
-
+    // ======== Hírek =============
     $output .= "<div id='fooldal_archiv'>\n";
     $archiv = scandir_safe_compact("content/archiv");
     $archiv_last = count($archiv) - 1;
@@ -46,9 +31,31 @@ $get_content = function(){
     $output .= $archiv_output;
     $output .= "<a href='?hely=archiv' class='backlink'>Régebbi események...</a>\n";
     $output .= "</div>\n<hr>\n";
-    $output .= "<script src='script/step_slideshow.js'></script>";
 
+    // ======= Képek =========
+    $output .= "<div id='fooldal_kepek'>\n";
+    if(include "slideshow.php"){
+        $galery = "img/galeria/";
+        $folder = "keszohidegkut_2022/";
+        $album = 41;
+        $title = "Keszőhidegkúti tábor 2022";
+        try{
+            $files = scandir_safe_compact($galery.$folder);
+            $output .= "<h3>$title</h3>";
+            $output .= slideshow($galery, $folder, $files, "", 0);
+            $output .= "<a href='?hely=galeria&album=$album' id='fooldal_kepek_link' title='$title'></a>\n";
+        }
+        catch(Exception $ex){
+                $output .= "<b>Error: </b>".$ex->getMessage();
+        }
+    }
+    $output .= "</div>\n<hr>\n";
+    
+    // ========= Aktuális eseméynek táblázat =======
     $output .= file_get_contents_safe("content/aktualis.html");
+
+    // ========== Script =======
+    $output .= "<script src='script/step_slideshow.js'></script>";
     
     return $output;
 };
