344ce54dbe1c8851b87aac9a5e8bc60a3b4f97d6
Gohér László <goher.laci@gmail.com>
2022-11-06 19:30:49 +0100

    Thumbnails everywhere for faster loading

--- script/archiv.php
+++ script/archiv.php
@@ -11,15 +11,15 @@ $get_content = function(){
     $output = "<h2>Hírek</h2>\n";
     $files = scandir_safe_compact("content/archiv/");
     for($i = count($files) - 1; $i >= 0; --$i){
-        $file = $files[$i];
-        $name = substr($file, 0, strpos($file, "."));
-        $output .= "<a href='?hely=hir&cim=$name' class='link_box archiv_box'>\n";
+        $file         = $files[$i];
+        $name         = substr($file, 0, strpos($file, "."));
+        $output       .= "<a href='?hely=hir&cim=$name' class='link_box archiv_box'>\n";
         $file_content = file_get_contents_safe("content/archiv/$file");
-        $img_src = get_image($file_content);
-        $output .= "<img src='$img_src' alt=''>\n";
-        $title = get_string_between($file_content, "<h2>", "</h2>");
-        $output .= "<span>$title</span>\n";
-        $output .= "</a>";
+        $img_src      = get_image($file_content);
+        $output       .= "<img src='$img_src' alt=''>\n";
+        $title        = get_string_between($file_content, "<h2>", "</h2>");
+        $output       .= "<span>$title</span>\n";
+        $output       .= "</a>";
     }
 
     return $output;
