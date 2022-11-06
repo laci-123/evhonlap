d5ab5fcfffc9ff26aab08e652f8796ebf4e86149
Gohér László <goher.laci@gmail.com>
2022-11-06 19:35:04 +0100

    Thumbnails even on the main page

--- script/fooldal.php
+++ script/fooldal.php
@@ -7,14 +7,14 @@ $get_content = function(){
     // ======= Aktuális alkalmak =========
     $files = scandir_safe_compact("content/aktualis/");
     foreach($files as $file){
-        $name = substr($file, 0, strpos($file, "."));
-        $output .= "<a href='?hely=esemeny&cim=$name' class='link_box aktualis_box'>\n";
-        $img_name = preg_replace("/^[0-9]*_/", "", $name);
-        $output .= "<img src='img/cikk/$img_name.png' alt=''>\n";
+        $name         = substr($file, 0, strpos($file, "."));
+        $output       .= "<a href='?hely=esemeny&cim=$name' class='link_box aktualis_box'>\n";
+        $img_name     = preg_replace("/^[0-9]*_/", "", $name);
+        $output       .= "<img src='img/cikk/Thumbnails/$img_name.png' alt=''>\n";
         $file_content = file_get_contents_safe("content/aktualis/$file");
-        $title = get_string_between($file_content, "<h2>", "</h2>");
-        $output .= "<span>$title</span>\n";
-        $output .= "</a>";
+        $title        = get_string_between($file_content, "<h2>", "</h2>");
+        $output       .= "<span>$title</span>\n";
+        $output       .= "</a>";
     }
     $output .= "<hr>\n";
 
