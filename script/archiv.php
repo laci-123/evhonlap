4ae9b51f1621e261f015a7ce167878066d4eaca5
Gohér László <goher.laci@gmail.com>
2023-02-18 18:54:20 +0100

    revorked how archiv.php works
    Instead of relying on the filenames to order the links
    and reading the beginning of files in 'content/archiv' to
    get the titles and thumbnails,
    now there is a separate file that explicitely lists all of these.

--- script/archiv.php
+++ script/archiv.php
@@ -1,25 +1,57 @@
 <?php
-function get_image($str){
-    $img_src = get_string_between($str, "<img src=\"", "\"");
-    if($img_src == ""){
-        $img_src = "img/allando/lutherrozsa.png";
-    }
-    return $img_src;
-}
-
 $get_content = function(){
     $output = "<h2>Hírek</h2>\n";
-    $files = scandir_safe_compact("content/archiv/");
-    for($i = count($files) - 1; $i >= 0; --$i){
-        $file         = $files[$i];
-        $name         = substr($file, 0, strpos($file, "."));
-        $output       .= "<a href='?hely=hir&cim=$name' class='link_box archiv_box'>\n";
-        $file_content = file_get_contents_safe("content/archiv/$file");
-        $img_src      = get_image($file_content);
-        $output       .= "<img src='$img_src' alt=''>\n";
-        $title        = get_string_between($file_content, "<h2>", "</h2>");
-        $output       .= "<span>$title</span>\n";
-        $output       .= "</a>";
+
+    $toc = file_get_contents_safe("content/archiv/archiv.txt");
+    $lines = preg_split("/\n/", $toc);
+
+    $filename = "";
+    $title = "";
+    $thumbnail = "";
+    $now_reading = "filename";
+
+    $default_thumbnail = "allando/lutherrozsa.png";
+
+    foreach($lines as $line){
+        if(preg_match("/^ *#.*/", $line)){
+            // ignore comments (lines staring with '#')
+            continue;
+        }
+
+        if(preg_match("/^ *$/", $line) and $now_reading != "emptyline"){
+            // ignore extra empty lines
+            continue;
+        }
+        
+        switch($now_reading){
+            case "filename":
+                $filename = $line;
+                
+                $now_reading = "title";
+                break;
+            case "title":
+                $title = $line;
+
+                $now_reading = "thumbnail";
+                break;
+            case "thumbnail":
+                $thumbnail = $line;
+
+                if($thumbnail == "?"){
+                    $thumbnail = $default_thumbnail;
+                }
+
+                $now_reading = "emptyline";
+                break;
+            case "emptyline":
+                $output .= "<a href='?hely=hir&cim=$filename' class='link_box archiv_box'>\n";
+                $output .= "    <img src='img/$thumbnail' alt=''>\n";
+                $output .= "    <span>$title</span>\n";
+                $output .= "</a>\n";
+
+                $now_reading = "filename";
+                break;
+        }
     }
 
     return $output;
