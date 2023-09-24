a353444aec81c188da05446da997faf019cdf75c
Gohér László <goher.laci@gmail.com>
2023-09-24 17:39:39 +0200

    fix formatting in menu.js

--- script/menu.js
+++ script/menu.js
@@ -2,12 +2,13 @@
 // either when clicked the X button or when clicked somewhere not on the menu
 
 const trigger = document.getElementById("trigger");
+
 document.addEventListener("click", function (event) {
     if (event.target.closest("#menu_lista") ||
-        event.target.closest("#trigger") ||
+        event.target.closest("#trigger")    ||
         event.target.closest("#next_slide") ||
         event.target.closest("#prev_slide") ||
-        event.target.tagName == "LABEL" ||
+        event.target.tagName == "LABEL"     ||
         event.target.tagName == "SPAN") 
     {
         return;
