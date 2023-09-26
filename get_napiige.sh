055fedbe285e57449e94d0dc18431cf9a08b14b0
Gohér László <goher.laci@gmail.com>
2023-09-26 14:31:28 +0200

    better implementation for get_napiige.sh

--- get_napiige.sh
+++ get_napiige.sh
@@ -1,19 +1,25 @@
 #!/usr/bin/sh
 
+# dependencies: pup (https://github.com/ericchiang/pup)
+
 set -e
 
 URL='https://www.evangelikus.hu/hitunk/lelki-taplalek?napiigenap='
-BOTH_OF_THEM='s/.*<h5>Napi igék<\/h5>\(.*\)<h5>Olvasmányok<\/h5>.*/\1/'
-FIRST_OF_TWO='s/\s*<p>\(.*\)<\/p>\s*<p>.*/\1/'
-SEPARATE='s/^\(.*\)(\(.*\))\s*$/\1\n\2/'
 
 start_date="$1"
 number_of_days="$2"
 
 for i in `seq 0 $number_of_days`
 do
-    datum=`date -d "$start_date $i days" +%Y-%m-%d`
+    tmp_file=$(mktemp)
+
+    datum=$(date -d "$start_date $i days" +%Y-%m-%d)
     echo "$datum:"
-    curl -s "$URL$datum" | tr '\n' ' ' | sed -e "$BOTH_OF_THEM" | sed -e "$FIRST_OF_TWO" | sed -e "$SEPARATE"
+    curl -s "$URL$datum" > "$tmp_file"
+    cat "$tmp_file" | pup 'h5:contains("Napi igék") + p text{}' | tr '\n' ' ' | sed 's/(.*)/\n/'
+    cat "$tmp_file" | pup 'h5:contains("Napi igék") + p > a'    | tr '\n' ' '
+
     printf "\n\n\n"
+
+    rm "$tmp_file"
 done
