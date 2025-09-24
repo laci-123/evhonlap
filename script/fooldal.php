198e1f2d0b0b45e765f4591727b70e4e62ff8900
Gohér László <goher.laci@gmail.com>
2025-09-24 17:42:04 +0200

    added new content

--- script/fooldal.php
+++ script/fooldal.php
@@ -4,6 +4,27 @@ $get_content = function(){
     $output =  file_get_contents_safe("content/fooldal.html");
     $output .= "<hr>\n";
 
+    // ======= Képek =========
+    if(include "slideshow.php"){
+        $output .= "<div id='fooldal_kepek'>\n";
+        $galery = "img/galeria/";
+        $folder = "ars_sacra_zaro_2025/";
+        $album = 56;
+        $title = "Ars Sacra Fesztivál 2025";
+        $files = scandir_safe_compact($galery.$folder);
+        $output .= "<h3>$title</h3>";
+        $output .= slideshow($galery, $folder, $files, "", 0);
+        $output .= "<a href='?hely=galeria&album=$album' id='fooldal_kepek_link' title='$title'></a>\n";
+        $output .= "</div>\n<hr>\n";
+    }
+        
+
+    // ========= Aktuális eseméynek táblázat =======
+    $output .= "<h3>Szeptemberi alkalmak</h3>\n";
+    $output .= file_get_contents_safe("content/aktualis.html");
+    $output .= "<hr>\n";
+
+
     // //======= Aktuális alkalmak =========
     // if(include "aktualis.php"){
     //     $output .= aktualis();
@@ -20,27 +41,6 @@ $get_content = function(){
     $output .= "</a>\n";
     $output .= "<hr>\n";
 
-
-    // ========= Aktuális eseméynek táblázat =======
-    $output .= "<h3>Szeptemberi alkalmak</h3>\n";
-    $output .= file_get_contents_safe("content/aktualis.html");
-    $output .= "<hr>\n";
-
-
-    // ======= Képek =========
-    if(include "slideshow.php"){
-        $output .= "<div id='fooldal_kepek'>\n";
-        $galery = "img/galeria/";
-        $folder = "gyulekezeti_kirandulas_tata/";
-        $album = 55;
-        $title = "Gyülekezeti kirándulás Tatán";
-        $files = scandir_safe_compact($galery.$folder);
-        $output .= "<h3>$title</h3>";
-        $output .= slideshow($galery, $folder, $files, "", 0);
-        $output .= "<a href='?hely=galeria&album=$album' id='fooldal_kepek_link' title='$title'></a>\n";
-        $output .= "</div>\n<hr>\n";
-    }
-        
 
     // ======= Ukrajna ========
     $output .=  file_get_contents_safe("content/ukrajna_fooldal.html");
