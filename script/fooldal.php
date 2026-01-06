f750dd879683e1a6754177bf26919cae0ece881b
Gohér László <goher.laci@gmail.com>
2026-01-06 14:09:02 +0100

    updated content

--- script/fooldal.php
+++ script/fooldal.php
@@ -11,7 +11,7 @@ $get_content = function(){
 
 
     // ========= Aktuális eseméynek táblázat =======
-    $output .= "<h3>Novemberi és decemberi alkalmak</h3>\n";
+    $output .= "<h3>Januári alkalmak</h3>\n";
     $output .= file_get_contents_safe("content/aktualis.html");
     $output .= "<hr>\n";
 
